be49f7308176caaa4fa21cad329ab2f3a515e32c
Imanda03 <asis03ktm@gmail.com>
2023-04-17 10:40:47 +0545

    calculator

--- lab10_a/app/src/main/java/com/example/lab10_a/First.java
+++ lab10_a/app/src/main/java/com/example/lab10_a/First.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,14 +19,16 @@ public class First extends AppCompatActivity {
     Button passButton;
     RadioGroup radioGroup;
     RadioButton radioButton;
+    CheckBox check1,check2,check3;
+    Spinner spinner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.passing_from);
 
-        Spinner spinner = (Spinner) findViewById(R.id.select);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.select        , android.R.layout.simple_spinner_item);
+        spinner = (Spinner) findViewById(R.id.select);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.select , android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
@@ -33,25 +36,38 @@ public class First extends AppCompatActivity {
         textBox2 = (EditText) findViewById(R.id.textBox2);
         passButton = (Button) findViewById(R.id.passButton);
         radioGroup = (RadioGroup) findViewById(R.id.rdGroup);
-
-
-
-
+         check1 = (CheckBox) findViewById(R.id.bca);
+         check2 = (CheckBox)findViewById(R.id.bbm);
+         check3 = (CheckBox)findViewById(R.id.bba);
         passButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 String str = textBox.getText().toString();
                 String str2 = textBox2.getText().toString();
+                String checkBoxResult = "";
+
+                if(check1.isChecked()){
+                    checkBoxResult += check1.getText().toString();
+                }
+                if(check2.isChecked()){
+                    checkBoxResult += check2.getText().toString();
+                }if(check3.isChecked()){
+                    checkBoxResult += check3.getText().toString();
+                }
+
                 int selectedID = radioGroup.getCheckedRadioButtonId();
 
                 radioButton = (RadioButton) findViewById(selectedID);
 
                 String getRadio = (String) radioButton.getText();
+                String getSpinner = (String) spinner.getSelectedItem();
 
                 Intent intent = new Intent(getApplicationContext(),Second.class);
                 intent.putExtra("message",str);
-                intent.putExtra("message",str2);
-                intent.putExtra("message",getRadio);
+                intent.putExtra("message2",str2);
+                intent.putExtra("radioChosen",getRadio);
+                intent.putExtra("checkBox",checkBoxResult);
+                intent.putExtra("spinner",getSpinner);
 
                 startActivity(intent);
             }
--- lab10_a/app/src/main/java/com/example/lab10_a/Second.java
+++ lab10_a/app/src/main/java/com/example/lab10_a/Second.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Second extends AppCompatActivity {
-    TextView text, text2, radioText;
+    TextView text, text2, radioText, checkboxText, spinnerText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,12 +18,18 @@ public class Second extends AppCompatActivity {
         text = (TextView) findViewById(R.id.text);
         text2 = (TextView) findViewById(R.id.text2);
         radioText = (TextView) findViewById(R.id.radio);
+        checkboxText = (TextView) findViewById(R.id.checkbox);
+        spinnerText = (TextView) findViewById(R.id.spinner);
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
-        String str2 = intent.getStringExtra("message");
-        String setRadio = intent.getStringExtra("message");
+        String str2 = intent.getStringExtra("message2");
+        String setRadio = intent.getStringExtra("radioChosen");
+        String setCheckbox = intent.getStringExtra("checkBox");
+        String spinner = intent.getStringExtra("spinner");
         text.setText(str);
         text2.setText(str2);
-        text2.setText(setRadio);
+        radioText.setText(setRadio);
+        checkboxText.setText(setCheckbox);
+        spinnerText.setText(spinner);
     }
 }
